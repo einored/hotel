3f6e00ee87cc02a966ec55d431da2e6c68d1f6ae
Einoras <58789065+einored@users.noreply.github.com>
2022-08-05 16:17:58 +0300

    sort, filter, search box

--- resources/views/hotel/index.blade.php
+++ resources/views/hotel/index.blade.php
@@ -5,6 +5,7 @@
 <div class="container">
     <div class="row justify-content-center">
         <div class="col">
+            {{-- @include('parts.box') --}}
             <div class="card">
                 <div class="card-header">Hotel list</div>
 
